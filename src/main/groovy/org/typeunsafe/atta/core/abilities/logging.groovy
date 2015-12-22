1a68fffbc501d6b82eab67ad2ae5dbfadb095e8c
ph.charriere@gmail.com <ph.charriere@gmail.com>
2015-12-22 23:40:10 +0100

    simple logging

---
+++ src/main/groovy/org/typeunsafe/atta/core/abilities/logging.groovy
@@ -0,0 +1,28 @@
+package org.typeunsafe.atta.core.abilities
+
+import java.time.Duration
+import java.time.LocalDateTime
+
+trait logging {
+  HashMap<String,LocalDateTime> tasks = new HashMap<>()
+
+  void startLog(String something) {
+    tasks.put(something, LocalDateTime.now())
+  }
+
+  Map updateLog(String something) {
+    LocalDateTime start = tasks.get(something)
+    LocalDateTime end = LocalDateTime.now()
+    Integer delay = Duration.between(start, end).toMillis()
+
+    println(
+        "-> " + something+" start:" + start.toLocalTime().toString()
+            + " end:" + end.toLocalTime().toString()
+            + " delay:" + delay + " ms"
+    )
+
+    return ["task":something, "start":start.toLocalTime(), "end":end.toLocalTime(), "delay":delay]
+
+  }
+
+}
